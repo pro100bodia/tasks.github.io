6dbcc1c0271c788bbcd493ddc4dd1149863c59df
Bodia <bohdanpukhalsky@ukr.net>
2018-05-20 17:33:39 +0300

    Commit made by Bodia

---
+++ calculator.c
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int addition(int x1, int x2){
+	return x1 + x2;
+}
+
+int main(){
+	int x1, x2;
+	printf("Input two figers: ");
+	scanf("%d %d", &x1, &x2);
+	int d = addition(x1, x2);
+	printf("%d", d);
+	return 0;
+}
