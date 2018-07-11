de329060f29b4f464d623334c85ebd4efb59c485
Artemiy <Artemiy.vlog@gmail.com>
2018-07-11 20:01:09 +0300

    sf

--- calculator.c
+++ calculator.c
@@ -4,11 +4,16 @@ int addition(int x1, int x2){
 	return x1 + x2;
 }
 
+int multiplication() {
+	return x1 * x2;
+}
+
 int main(){
 	int x1, x2;
 	printf("Input two figers: ");
 	scanf("%d %d", &x1, &x2);
 	int d = addition(x1, x2);
+	int d = multiplication(x1, x2);
 	printf("%d", d);
 	return 0;
 }
